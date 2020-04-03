956299551717379fcb4a3406b9c9c18b0a3e84b4
Sebastian Dittert <Sebastian.dittert@gmx.de>
2020-04-03 12:25:47 +0200

    Add files via upload

--- dqn_test.py
+++ dqn_test.py
@@ -11,18 +11,9 @@ from torch.utils.tensorboard import SummaryWriter
 import gym
 import argparse 
 import time
-import cv2
+import matplotlib.pyplot as plt
 
-def preprocess_img(state):
-    gray = cv2.cvtColor(state, cv2.COLOR_RGB2GRAY)
-    resized = cv2.resize(gray, (84,110))
-    cropped = resized[18:102,:]
-    cropped = cropped.reshape((1,84,84))
-    return cropped
-
-
-
-def run(n_episodes=1000, max_t=1000, eps_start=1.0, eps_end=0.01, eps_decay=0.99):
+def run(n_episodes=1000, eps_frames=1e6, min_eps=0.01):
     """Deep Q-Learning.
     
     Params
@@ -35,32 +26,31 @@ def run(n_episodes=1000, max_t=1000, eps_start=1.0, eps_end=0.01, eps_decay=0.99
     """
     scores = []                        # list containing scores from each episode
     scores_window = deque(maxlen=100)  # last 100 scores
-    eps = eps_start                    # initialize epsilon
+    eps = 1
+    eps_start = 1
+    frame = 0                  
     for i_episode in range(1, n_episodes+1):
         state = env.reset()
-        #state = preprocess_img(state)
-        #state = np.vstack([state, state, state, state])
-
+        plt.show()
         score = 0
         while True:
             action = agent.act(state, eps)
             next_state, reward, done, _ = env.step(action)
-            #next_state = preprocess_img(next_state)
-            #next_state = np.concatenate((next_state, state[:3,:,:]))
             agent.step(state, action, reward, next_state, done)
             state = next_state
             score += reward
+            frame += 1
             if done:
                 break 
         scores_window.append(score)       # save most recent score
         scores.append(score)              # save most recent score
-        eps = max(eps_end, eps_decay*eps) # decrease epsilon
+        eps = max(eps_start - (frame*(1/eps_frames)), min_eps)
         writer.add_scalar("Epsilon", eps, i_episode)
         writer.add_scalar("Reward", score, i_episode)
         writer.add_scalar("Average100", np.mean(scores_window), i_episode)
-        print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_window)), end="")
+        print('\rEpisode {}\tFrame {} \tAverage Score: {:.2f}'.format(i_episode, frame, np.mean(scores_window)), end="")
         if i_episode % 100 == 0:
-            print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_window)))
+            print('\rEpisode {}\tFrame {}\tAverage Score: {:.2f}'.format(i_episode,frame, np.mean(scores_window)))
 
     return scores
 
@@ -68,7 +58,7 @@ def run(n_episodes=1000, max_t=1000, eps_start=1.0, eps_end=0.01, eps_decay=0.99
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-agent", type=str, choices=["dqn, ddqn, duel"], default="dqn", help="Specify which type of DQN agent you want to train, default is DQN - baseline!")
-    parser.add_argument("-env", type=str, default="Pong-v0", help="Name of the atari Environment, default = Pong-v0")
+    parser.add_argument("-env", type=str, default="PongDeterministic-v4", help="Name of the atari Environment, default = Pong-v0")
     parser.add_argument("-eps", type=int, default=500, help="Number of Episodes to train, default = 500")
     parser.add_argument("-seed", type=int, default=1, help="Random seed to replicate training runs, default = 1")
     parser.add_argument("-bs", "--batch_size", type=int, default=32, help="Batch size for updating the DQN, default = 32")
@@ -77,11 +67,10 @@ if __name__ == "__main__":
     parser.add_argument("-lr", type=float, default=1e-3, help="Learning rate, default = 1e-3")
     parser.add_argument("-g", "--gamma", type=float, default=0.99, help="Discount factor gamma, default = 0.99")
     parser.add_argument("-t", "--tau", type=float, default=1e-3, help="Soft update parameter tat, default = 1e-3")
-    parser.add_argument("-ep_start", type=float, default=1.0, help="Epsilon greedy starting value, default = 1")
-    parser.add_argument("-ep_decay", type=float, default=0.99, help="Epsilon greedy decay value, default = 0.99")
-    parser.add_argument("-ep_end", type=float, default = 0.05, help="Final epsilon greedy value, default = 0.05")
+    parser.add_argument("-eps_frames", type=int, default=1e6, help="Linear annealed frames for Epsilon, default = e6")
+    parser.add_argument("-min_eps", type=float, default = 0.1, help="Final epsilon greedy value, default = 0.1")
     parser.add_argument("-info", type=str, help="Name of the training run")
-    parser.add_argument("-save_mode", type=int, choices=[0,1], default=0, help="Specify if the trained network shall be saved or not, default is 0 - not saved!")
+    parser.add_argument("-save_model", type=int, choices=[0,1], default=0, help="Specify if the trained network shall be saved or not, default is 0 - not saved!")
 
     args = parser.parse_args()
     writer = SummaryWriter("runs/"+str(args.info))
@@ -105,7 +94,7 @@ if __name__ == "__main__":
 
     agent = DQN_Agent(state_size=state_size, action_size=action_size, BATCH_SIZE=BATCH_SIZE, BUFFER_SIZE=BUFFER_SIZE, LR=LR, TAU=TAU, GAMMA=GAMMA,UPDATE_EVERY=UPDATE_EVERY, device=device, seed=seed)
     t0 = time.time()
-    scores = run(n_episodes = args.eps, eps_start=args.ep_start, eps_end=args.ep_end, eps_decay=args.ep_decay)
+    scores = run(n_episodes = args.eps, eps_frames=args.eps_frames, min_eps=args.min_eps)
     t1 = time.time()
     print("Training time: {}min".format((t1-t0)/60))
     if args.save_mode:
