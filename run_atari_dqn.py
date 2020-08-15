0c3c17ae976d83b8b7216e1fa3c6d0fad2f685e1
BY571 <sebastian.dittert@gmx.de>
2020-08-15 14:54:11 +0200

    change evaluation schedule

--- run_atari_dqn.py
+++ run_atari_dqn.py
@@ -12,6 +12,28 @@ import gym
 import argparse 
 import time
 
+def evaluate(eps, frame, eval_runs=5):
+    """
+    Makes an evaluation run with the current epsilon
+    """
+    if "-ram" in env_name or env_name == "CartPole-v0" or env_name == "LunarLander-v2": 
+        env = gym.make(env_name)
+    else:
+        env = wrapper_new.make_env(env_name)
+    reward_batch = []
+    for i in range(eval_runs):
+        state = env.reset()
+        rewards = 0
+        while True:
+            action = agent.act(state, eps)
+            state, reward, done, _ = env.step(action)
+            rewards += reward
+            if done:
+                break
+        reward_batch.append(rewards)
+        
+    writer.add_scalar("Reward", np.mean(reward_batch), frame)
+
 
 def run_random_policy(random_frames):
     """
@@ -26,16 +48,17 @@ def run_random_policy(random_frames):
         if done:
             state = env.reset()
 
-def run(frames=1000, eps_fixed=False, eps_frames=1e6, min_eps=0.01):
+def run(frames=1000, eps_fixed=False, eps_frames=1e6, min_eps=0.01, eval_every=1000, eval_runs=5):
     """Deep Q-Learning.
     
     Params
     ======
-        n_episodes (int): maximum number of training episodes
-        max_t (int): maximum number of timesteps per episode
-        eps_start (float): starting value of epsilon, for epsilon-greedy action selection
-        eps_end (float): minimum value of epsilon
-        eps_decay (float): multiplicative factor (per episode) for decreasing epsilon
+        frames (int): maximum number of training frames
+        eps_fixed (bool): training with greedy policy and noisy layer (fixed) or e-greedy policy (not fixed)
+        eps_frames (float): number of frames to decay epsilon exponentially
+        min_eps (float): minimum value of epsilon from where eps decays linear until the last frame
+        eval_every (int): number frames when evaluation runs are done
+        eval_runs (int): number of evaluation runs
     """
     scores = []                        # list containing scores from each episode
     scores_window = deque(maxlen=100)  # last 100 scores
@@ -62,11 +85,13 @@ def run(frames=1000, eps_fixed=False, eps_frames=1e6, min_eps=0.01):
             else:
                 eps = max(min_eps - min_eps*((frame-eps_frames)/(frames-eps_frames)), 0.001)
 
+        # evaluation runs
+        if frame % eval_every == 0:
+            evaluate(eps, frame, eval_runs)
+
         if done:
             scores_window.append(score)       # save most recent score
             scores.append(score)              # save most recent score
-            writer.add_scalar("Epsilon", eps, i_episode)
-            writer.add_scalar("Reward", score, i_episode)
             writer.add_scalar("Average100", np.mean(scores_window), i_episode)
             print('\rEpisode {}\tFrame {} \tAverage Score: {:.2f}'.format(i_episode, frame, np.mean(scores_window)), end="")
             if i_episode % 100 == 0:
@@ -105,11 +130,13 @@ if __name__ == "__main__":
     parser.add_argument("-layer_size", type=int, default=512, help="Size of the hidden layer, default=512")
     parser.add_argument("-n_step", type=int, default=1, help="Multistep DQN, default = 1")
     parser.add_argument("-m", "--memory_size", type=int, default=int(1e5), help="Replay memory size, default = 1e5")
-    parser.add_argument("-u", "--update_every", type=int, default=4, help="Update the network every x steps, default = 4")
+    parser.add_argument("-u", "--update_every", type=int, default=1, help="Update the network every x steps, default = 1")
     parser.add_argument("-lr", type=float, default=0.00025, help="Learning rate, default = 0.00025")
     parser.add_argument("-g", "--gamma", type=float, default=0.99, help="Discount factor gamma, default = 0.99")
     parser.add_argument("-t", "--tau", type=float, default=1e-3, help="Soft update parameter tat, default = 1e-3")
     parser.add_argument("-eps_frames", type=int, default=150000, help="Linear annealed frames for Epsilon, default = 150000")
+    parser.add_argument("-eval_every", type=int, default=50000, help="Evaluate every x frames, default = 50000")
+    parser.add_argument("-eval_runs", type=int, default=5, help="Number of evaluation runs, default = 5")
     parser.add_argument("-min_eps", type=float, default = 0.1, help="Final epsilon greedy value, default = 0.1")
     parser.add_argument("-info", type=str, help="Name of the training run")
     parser.add_argument("--fill_buffer", type=int, default=50000, help="Adding samples to the replay buffer based on a random policy, before agent-env-interaction. Input numer of preadded frames to the buffer, default = 50000")
@@ -127,17 +154,19 @@ if __name__ == "__main__":
     GAMMA = args.gamma
     TAU = args.tau
     LR = args.lr
+    seed = args.seed
     UPDATE_EVERY = args.update_every
     n_step = args.n_step
+    env_name = args.env
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     print("Using ", device)
 
 
     np.random.seed(seed)
-    if "-ram" in args.env or args.env == "CartPole-v0" or args.env == "LunarLander-v2": 
-        env = gym.make(args.env)
+    if "-ram" in env_name or env_name == "CartPole-v0" or env_name == "LunarLander-v2": 
+        env = gym.make(env_name)
     else:
-        env = wrapper_new.make_env(args.env)
+        env = wrapper_new.make_env(env_name)
 
     env.seed(seed)
     action_size     = env.action_space.n
@@ -184,7 +213,7 @@ if __name__ == "__main__":
         eps_fixed = False
 
     t0 = time.time()
-    final_average100 = run(frames = args.frames, eps_fixed=eps_fixed, eps_frames=args.eps_frames, min_eps=args.min_eps)
+    final_average100 = run(frames = args.frames, eps_fixed=eps_fixed, eps_frames=args.eps_frames, min_eps=args.min_eps, eval_every=args.eval_every, eval_runs=args.eval_runs)
     t1 = time.time()
     
     print("Training time: {}min".format(round((t1-t0)/60,2)))
